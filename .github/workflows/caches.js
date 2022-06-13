864fcdd3c965838b29cf87c98cb551235ab23ca0
Vincent Clemson <vclemson07@gmail.com>
2022-06-13 02:52:12 -0400

    init | use ktmud/cached-dependencies to setup multiple cache restore on schedule

---
+++ .github/workflows/caches.js
@@ -0,0 +1,12 @@
+module.exports = {
+  knitr: {
+    path: [`${process.cwd()}/'knitr-cache_cache`],
+    hashFiles: [''],
+    restoreKeys: `${OS_VERSION}-${R_VERSION}-__prncevince_r-actions_2-${WORKFLOW}-`
+  },
+  renv: {
+    path: [`${TMPDIR}/renv`],
+    hashFiles: [''],
+    restoreKeys: `${OS_VERSION}-${R_VERSION}-__prncevince_r-actions-${WORKFLOW}-`
+  }
+}
---
+++ build.R
@@ -0,0 +1,6 @@
+rmarkdown::render(
+  input = "knitr-cache.Rmd",
+  output_dir = "docs/",
+  output_file = "index.html",
+  output_format = "html_document"
+)
