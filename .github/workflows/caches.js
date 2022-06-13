15153a22ef0f32f02cd3751ae2c6abd14881ded1
Vincent Clemson <vclemson07@gmail.com>
2022-06-13 04:00:51 -0400

    add dummy bashlib.sh file | change environment variable syntax to node process.env. syntax

--- .github/workflows/caches.js
+++ .github/workflows/caches.js
@@ -2,11 +2,11 @@ module.exports = {
   knitr: {
     path: [`${process.cwd()}/knitr-cache_cache`],
     hashFiles: [''],
-    restoreKeys: `${OS_VERSION}-${R_VERSION}-__prncevince_r-actions_2-${WORKFLOW}-`
+    restoreKeys: `${process.env.OS_VERSION}-${process.env.R_VERSION}-__prncevince_r-actions_2-${process.env.WORKFLOW}-`
   },
   renv: {
-    path: [`${TMPDIR}/renv`],
+    path: [`${process.env.TMPDIR}/renv`],
     hashFiles: [''],
-    restoreKeys: `${OS_VERSION}-${R_VERSION}-__prncevince_r-actions-${WORKFLOW}-`
+    restoreKeys: `${process.env.OS_VERSION}-${process.env.R_VERSION}-__prncevince_r-actions-${process.env.WORKFLOW}-`
   }
 }
